628a8550f02ada324977b7f5811cc8283aa29772
RyanTheNerd <509528@gmail.com>
2019-06-22 17:07:35 -0700

    Added a stats page that lists everybody's names

---
+++ animate.py
@@ -0,0 +1,53 @@
+import asyncio
+from utils import parse_request
+import json
+
+class Animation:
+    def __init__(self, trigger, frame_delay, frames, iterations, monospaced=False):
+        self.monospaced = monospaced
+        self.trigger = trigger
+        self.frames = frames
+        self.frame_delay = frame_delay
+        self.iterations = iterations
+    
+    async def animate(self, channel):
+        original = await channel.send(self.frames[0])
+        for iteration in range(self.iterations):
+            for frame in self.frames:
+                if self.monospaced:
+                    frame = '```' + frame + '```'
+                await original.edit(content = frame)
+                await asyncio.sleep(self.frame_delay)
+
+    async def run(self, message):
+        user, command, arg = parse_request(message)
+        if command == self.trigger:
+            await self.animate(message.channel)
+
+
+
+
+smile_wink_frames = [
+    ":smiley:",
+    ":wink:",
+]
+sad_cry_frames = [
+    ":frowning:",
+    ":sob:",
+]
+
+with open('porn_frames.json', 'r') as porn_frame_file:
+    porn_frames = json.load(porn_frame_file)
+
+
+animations = [
+    Animation("smile", 1, smile_wink_frames, 5),
+    Animation("cry", 1, sad_cry_frames, 5),
+    Animation("porn", 1, porn_frames, 1, monospaced = True),
+]
+
+async def run_animations(message):
+    for animation in animations:
+        await animation.run(message)
+
+
---
+++ deepfry.py
@@ -0,0 +1,4 @@
+from PIL import Image
+def deepfry(imgPath):
+    img = Image.open(imgPath)
+
